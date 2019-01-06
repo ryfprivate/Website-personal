b00e387d8cf3e02f3d286ba97d8cc247243ad1c5
yuetongfeng <ryfprivate@outlook.com>
2019-01-07 00:02:36 +1100

    Added SPA functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,37 @@
-$(function() {
-    // Using core $.ajax() method
-    $.ajax({
-        // the URL for the request
-        url: "./assets/pages/about.html",
+$(function () {
+    // On-click function for link element to the about page
+    $(".link--about").on("click", function (e) {
+        // Prevents the link from going to another page
+        e.preventDefault();
+        // Finds the link of the particular page
+        var pageRef = $(this).attr("href");
+        callPage(pageRef);
+    });
 
-        // whether this is a POST or GET request
-        type: "GET",
-
-        // the type of data we expect back
-        dataType: "text",
-
-        // Code to run if the request succeeds;
-        // the response is passed to the function
-        success: function(response) {
-            console.log("the page was loaded", response);
-            $(".content").html(response);
-        },
-        // Code to run if the request fails; the raw request and
-        // status codes are passed to the function
-        error: function(error) {
-            console.log("the page was NOT loaded", error);
-        },
-        // Code to run regardless of success or failure
-        complete: function(xhr, status) {
-            console.log("THe request is complete!");
-        }
-    })
+    function callPage(pageRefInput) {
+        // Using core $.ajax() method, calls the content of requested page
+        $.ajax({
+            // the URL for the request
+            url: pageRefInput,
+            // whether this is a POST or GET request
+            type: "GET",
+            // the type of data we expect back
+            dataType: "text",
+            // Code to run if the request succeeds;
+            // the response is passed to the function
+            success: function (response) {
+                console.log("the page was loaded", response);
+                $(".content").html(response);
+            },
+            // Code to run if the request fails; the raw request and
+            // status codes are passed to the function
+            error: function (error) {
+                console.log("the page was NOT loaded", error);
+            },
+            // Code to run regardless of success or failure
+            complete: function (xhr, status) {
+                console.log("THe request is complete!");
+            }
+        });
+    }
 });
